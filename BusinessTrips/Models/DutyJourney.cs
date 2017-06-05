4201837a8b2317724c4fed2da227a661ef9da1ac
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-06-05 22:27:32 +0300

    Добавлен проект: BusinessTrips. Созданы классы моделей: DutyJourney (командировка), Employee (сотрудник).

---
+++ BusinessTrips/Models/DutyJourney.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessTrips.Models
+{
+    public class DutyJourney
+    {
+        int Id { get; set; }
+        public string WorkDay { get; set; }
+        public string FreeDay { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Point { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+        public DutyJourney()
+        {
+            Employees = new List<Employee>();
+        }
+    }
+}
---
+++ BusinessTrips/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessTrips.Models
+{
+    public class Employee
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Surname { get; set; }
+        string Lastname { get; set; }
+
+        public virtual ICollection<DutyJourney> DutyJourneys { get; set; }
+        public Employee()
+        {
+            DutyJourneys = new List<DutyJourney>();
+        }
+    }
+}
