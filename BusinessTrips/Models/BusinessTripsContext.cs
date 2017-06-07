85c26d6724c2d6c1f7f24695090235583d191391
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-06-07 04:20:55 +0300

    1. Предварительно сгенерирован контроллер с набором базовых вьюшек для модели: Emploees. :) 2. Поправлен файл: Web.config, разобрался с человеческим подключением к БД, когда работа идёт через CF. 3. В представление: _Layout.cshtml добавлена ссылка на Index-вьюху модели: Emploees. 4. Класс: BusinessTripsContext разделён на два, выделен отдельно от него класс: BusinessTripsDbInitializer, который отвечает за заполнение дефолтными значениями модели БД. 5. В класс: Global.asax.cs добавлена строчка для корректной иннициализации БД.

--- BusinessTrips/Models/BusinessTripsContext.cs
+++ BusinessTrips/Models/BusinessTripsContext.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
@@ -11,18 +10,7 @@ namespace BusinessTrips.Models
     {
         public DbSet<Employee> Employees { get; set; }
         public DbSet<DutyJourney> DutyJourneys { get; set; }
-    }
-    public class BusinessTripsDbInitializer : DropCreateDatabaseAlways<BusinessTripsContext>
-    {
-        protected override void Seed(BusinessTripsContext context)
-        {
-            Employee empl1 = new Employee { Id = 1, Name = "Ivan", Surname = "Ivanovich", Lastname = "Ivanov" };
-            DutyJourney dj1 = new DutyJourney { Id = 1, City = "Tomsk", Country = "Russia", Point = "Professioanl courses", FreeDay = "Monday", WorkDay = "Sunday", Employees = new List<Employee>() { empl1 } };
 
-            context.Employees.Add(empl1);
-            context.DutyJourneys.Add(dj1);
-
-            context.SaveChanges();
-        }
+        public BusinessTripsContext() : base("BusTripConnection") { }
     }
 }
