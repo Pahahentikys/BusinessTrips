0e3b73b27709e6f411b63d3e3064318806ad11ee
Mikhail1488 <mikha-khar@yandex.ru>
2017-06-08 17:05:23 +0700

    Сгенерировали контроллер для Passege.Изменил файл мастера страницы.

---
+++ BusinessTrips/Controllers/PassagesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BusinessTrips.Models;
+
+namespace BusinessTrips.Controllers
+{
+    public class PassagesController : Controller
+    {
+        private BusinessTripsContext db = new BusinessTripsContext();
+
+        // GET: Passages
+        public ActionResult Index()
+        {
+            return View(db.Passeges.ToList());
+        }
+
+        // GET: Passages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Passage passage = db.Passeges.Find(id);
+            if (passage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(passage);
+        }
+
+        // GET: Passages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Passages/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,DateofDeparture,DateofArrival,Transport,DestinationPoint,DeparturePoint")] Passage passage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Passeges.Add(passage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(passage);
+        }
+
+        // GET: Passages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Passage passage = db.Passeges.Find(id);
+            if (passage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(passage);
+        }
+
+        // POST: Passages/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,DateofDeparture,DateofArrival,Transport,DestinationPoint,DeparturePoint")] Passage passage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(passage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(passage);
+        }
+
+        // GET: Passages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Passage passage = db.Passeges.Find(id);
+            if (passage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(passage);
+        }
+
+        // POST: Passages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Passage passage = db.Passeges.Find(id);
+            db.Passeges.Remove(passage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
