dcdc1024db9327418a1f156c113c0d52bdcc861d
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-06-15 04:38:02 +0300

    Проба работы аутентификации/авторизации через ASP identity. Поправлен connection в файле модели: IdentityModel.cs.

--- BusinessTrips/Models/IdentityModels.cs
+++ BusinessTrips/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace BusinessTrips.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("BusTripConnection", throwIfV1Schema: false)
         {
         }
 
